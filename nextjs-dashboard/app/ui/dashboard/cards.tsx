3600d7b4deffa3b586df3272a6ea2f08be3dcdb0
phillipeismark <phillip_eismark@hotmail.dk>
2024-01-14 19:04:50 +0100

    chapter 10 partial rendering

--- nextjs-dashboard/app/ui/dashboard/cards.tsx
+++ nextjs-dashboard/app/ui/dashboard/cards.tsx
@@ -5,7 +5,7 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
-
+import { fetchCardData } from '@/app/lib/data';
 const iconMap = {
   collected: BanknotesIcon,
   customers: UserGroupIcon,
@@ -14,19 +14,24 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
-  return (
-    <>
-      {/* NOTE: comment in this code when you get to this point in the course */}
+  const {
+    totalPaidInvoices,
+    totalPendingInvoices,
+    numberOfInvoices,
+    numberOfCustomers,
+  } = await fetchCardData();
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+  return (
+    <div className="flex flex-row justify-between">
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
-    </>
+      />
+    </div>
   );
 }
 
